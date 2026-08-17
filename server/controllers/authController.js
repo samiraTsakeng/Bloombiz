cd5373e68bf3159ff88cc282508c10b0197b34dc
samiraTsakeng <tsakengsamira@gmail.com>
2026-08-17 14:15:41 +0100

    Implement user login and JWT generation

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import generateToken from "../utils/generateToken.js";
 
 export const registerUser = async (req, res) => {
     try {
@@ -67,6 +68,75 @@ export const registerUser = async (req, res) => {
 
         res.status(500).json({
             message: "An error occured while creating your account. please try later."
+        });
+    }
+};
+
+export const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        //1. vaalidate input
+        if (!email || !password) {
+            return res.status(400).json({
+                message: "Please provide your email and password."
+            });
+        }
+
+        //2. find the user
+        const user = await User.findOne({
+            email: email.toLowerCase()
+        });
+
+        if (!user) {
+            return res.status(401).json({
+                message: "Invalid email or password."
+            })
+        }
+
+        //3. compare the password
+        const isPasswordCorrect = await bcrypt.compare(
+            password,
+            user.password
+        );
+
+        if (!isPasswordCorrect) {
+            return res.status(401).json({
+                message: "Invalid email or password."
+            });
+        }
+
+        //4. check account status
+        if (user.status !== "active") {
+            return res.status(403).json({
+                message: "your account is currently unavailable"
+            });
+        }
+
+        //5. generate JWT
+        const token = generateToken(user._id.toString());
+
+        //6. send response
+        res.status(200).json({
+            message: "Login successful.",
+            token,
+            user: {
+                id: user._id,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.email,
+                businessName: user.businessName,
+                category: user.category,
+                phoneNumber: user.phoneNumber,
+                avatar: user.avatar
+            }
+        });
+
+    } catch (error) {
+        console.error("Login error:", error);
+
+        res.status(500).json({
+            message: "An error occured while logging in."
         });
     }
 };
